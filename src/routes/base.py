4285c472e381cf8f8bbbff533ef0d526b2c38cb5
Mahmoud-Khater <mahmoudgamalkhater@gmail.com>
2025-08-10 22:45:19 +0300

    last

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings , Settings
+
 base_router = APIRouter(
 	prefix="/api/v1",
 	tags=["rag"],
 )
 
 @base_router.get("/")
-async def welcome():
-	app_name = os.getenv('APP_NAME')
+async def welcome(app_settings : Settings = Depends(get_settings)):
+	# app_settings = get_settings()
+
+	app_name = app_settings.APP_NAME
 	return {
 		"APP_NAME" : app_name
 	}
