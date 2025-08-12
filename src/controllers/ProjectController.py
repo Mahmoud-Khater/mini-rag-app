1c8185abeef22eb16a96bf480ada8601feb1b86e
Mahmoud-Khater <mahmoudgamalkhater@gmail.com>
2025-08-12 17:40:50 +0300

    upload file

---
+++ src/controllers/ProjectController.py
@@ -0,0 +1,20 @@
+from .BaseController import BaseController
+from models import ResponseSignal
+from fastapi import UploadFile
+import os
+
+class ProjectController(BaseController):
+    
+    def __init__(self):
+        super().__init__()
+    
+    def getProjectPath(self, project_id : str):
+        project_dir = os.path.join(
+            self.files_dir,
+            project_id
+        )
+
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+
+        return project_dir
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, APIRouter, Depends , UploadFile
+from fastapi import FastAPI, APIRouter, Depends , UploadFile , status
+from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings , Settings
-from controllers import DataController
+from controllers import DataController, ProjectController
+import aiofiles
+from models import ResponseSignal
 
 data_router = APIRouter(
 	prefix="/api/v1/data",
@@ -14,6 +17,27 @@ async def upload_data(project_id : str, file: UploadFile,
     #validate
     is_valid, signal = DataController().validate_file(file=file)
 
-    return {
-        "signal" : signal
-    }
+    if not is_valid:
+        return JSONResponse (
+            status_code = status.HTTP_300_MULTIPLE_CHOICES,
+            content = {
+                "signal" : signal
+            }
+        )
+    
+    project_dir_path = ProjectController().get_project_path(project_id = project_id)
+
+    file_path = os.path.join(
+        project_dir_path,
+        file.filename
+    )
+
+    async with aiofiles.open(file_path,"wb") as f:
+        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+            await f.write(chunk)
+    
+    return JSONResponse(
+        content={
+            "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value
+        }
+    )
